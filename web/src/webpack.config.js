578de751bb1237fee599fef8c0e89d429bd225b1
Stan <tangjianyi1993@live.com>
2018-03-29 17:40:00 +0800

    update：添加部分前端 ui 库、js 库，用于 html 页面引入

--- web/src/webpack.config.js
+++ web/src/webpack.config.js
@@ -3,7 +3,7 @@
 const path = require('path');
 
 module.exports = {
-    entry: './index.js',
+    entry: './app.js',
     output: {
         path: path.resolve(__dirname, '../public'),
         filename: 'index.js',
