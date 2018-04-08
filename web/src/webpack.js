22e5d70de10e9cf116c7aef5f48c70e378c2d4bd
Stan <tangjianyi1993@live.com>
2018-04-08 16:38:59 +0800

    update：添加打包的相关信息——开始时间、结束时间、打包耗时

--- web/src/webpack.js
+++ web/src/webpack.js
@@ -1,15 +1,28 @@
 'use strict';
 
+const moment = require('moment');
+
 const webpack = require('webpack');
 
 const config = require('./webpack.config');
+/* eslint-disable */
+webpack(config, (err, stats) => {
+    const startDate = moment(stats.startTime).format('YYYY-MM-DD HH:mm:ss');
+    const endDate = moment(stats.endTime).format('YYYY-MM-DD HH:mm:ss');
+    const compileTime = (stats.endTime - stats.startTime) / 1000;
 
-webpack(config, (err) => {
+    console.info('\x1b[36m%s\x1b[0m', '----- BUNDLE -----');
+    console.info('\x1b[36m%s\x1b[0m', ` start time: ${startDate}`);
+    console.info('\x1b[36m%s\x1b[0m', `   end time: ${endDate}`);
+    console.info('\x1b[36m%s\x1b[0m', `bundle time: ${compileTime} s`);
+    console.info('\x1b[36m%s\x1b[0m', '------------------');
+
     if (err) {
-        console.info('----- ERROR -----');  //  eslint-disable-line
-        console.info(err);  //  eslint-disable-line
-        console.info('-----------------');  //  eslint-disable-line
+        console.info('----- ERROR -----');
+        console.info('\x1b[36m%s\x1b[0m', err);
+        console.info('-----------------');
     }
 
     return true;
 });
+/* eslint-disable */
