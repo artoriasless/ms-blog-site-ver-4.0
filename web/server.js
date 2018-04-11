aa802d3f48189005261065103c23d6d842b9f608
Stan <tangjianyi1993@live.com>
2018-04-11 10:03:04 +0800

    update：添加引入 koa-logger 日志中间件

--- web/server.js
+++ web/server.js
@@ -7,6 +7,7 @@ const config = require('../config');
 const koaStatic = require('koa-static');
 const koaBody = require('koa-bodyparser');
 const koaSession = require('koa-session');
+const koaLogger = require('koa-logger');
 const Koa = require('koa');
 const app = new Koa();
 
@@ -18,8 +19,9 @@ app.keys = config.sessionKeys;
 app.use(koaSession(config.session, app))
     .use(koaStatic(path.resolve(__dirname, './public')))
     .use(koaBody())
+    .use(koaLogger())
+    .use(globalException)
     .use(router.routes())
-    .use(router.allowedMethods())
-    .use(globalException);
+    .use(router.allowedMethods());
 
 app.listen(config.port);
