2af36faa64516233056d56ce6e127e7a9d3da83f
Stan <tangjianyi1993@live.com>
2018-04-25 17:46:01 +0800

    update：更新打包输出信息

--- web/src/webpack.js
+++ web/src/webpack.js
@@ -48,26 +48,27 @@ webpack(config, (err, stats) => {
         });
     }
 
-    const startDate = moment(stats.startTime).format('YYYY-MM-DD HH:mm:ss');
-    const endDate = moment(stats.endTime).format('YYYY-MM-DD HH:mm:ss');
-    const compileTime = (stats.endTime - stats.startTime) / 1000;
-
-    console.info('\x1b[36m%s\x1b[0m', '----- BUNDLE -----');
-    console.info('\x1b[36m%s\x1b[0m', ` start time: ${startDate}`);
-    console.info('\x1b[36m%s\x1b[0m', `   end time: ${endDate}`);
-    console.info('\x1b[36m%s\x1b[0m', `bundle time: ${compileTime} s`);
-
     if (err) {
         console.info('----- ERROR -----');
         console.info('\x1b[36m%s\x1b[0m', err);
         console.info('-----------------');
     } else {
+        /* 打包时的错误信息：语法等 */
         const errors = stats.compilation.errors;
         if (Object.keys(errors).length > 0) {
             console.info('\x1b[36m%s\x1b[0m', '----- ERROR -----');
             console.info(errors);
         }
+        /* 打包信息：耗时、起止时间 */
+        const startDate = moment(stats.startTime).format('YYYY-MM-DD HH:mm:ss');
+        const endDate = moment(stats.endTime).format('YYYY-MM-DD HH:mm:ss');
+        const compileTime = (stats.endTime - stats.startTime) / 1000;
 
+        console.info('\x1b[36m%s\x1b[0m', '----- BUNDLE -----');
+        console.info('\x1b[36m%s\x1b[0m', ` start time: ${startDate}`);
+        console.info('\x1b[36m%s\x1b[0m', `   end time: ${endDate}`);
+        console.info('\x1b[36m%s\x1b[0m', `bundle time: ${compileTime} s`);
+        /* 静态资源添加版本号信息，击穿缓存 */
         const tempDate = new Date();
         const fileDist = path.resolve(__dirname, '../public');
 
