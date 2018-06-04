3719cbf11c02f10410ac6e2cdc422e218f3705a2
Stan <tangjianyi1993@live.com>
2018-06-04 20:06:00 +0800

    update：添加登录注册 tab 切换

--- web/public/index.js
+++ web/public/index.js
@@ -339,6 +339,7 @@ module.exports = Navbar;
 
 "use strict";
 
+/* global $ */
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
@@ -352,6 +353,8 @@ var React = __webpack_require__(/*! react */ "./node_modules/react/index.js");
 /* eslint-disable */
 var LoginForm = __webpack_require__(/*! ./login-modal-login-form */ "./components/ui-components/login-modal-login-form.js");
 var RegisterForm = __webpack_require__(/*! ./login-modal-register-form */ "./components/ui-components/login-modal-register-form.js");
+
+__webpack_require__(/*! ../../plugins/switch-button/js/index.js */ "./plugins/switch-button/js/index.js");
 /* eslint-disable */
 
 var LoginModalBody = function (_React$Component) {
@@ -369,46 +372,27 @@ var LoginModalBody = function (_React$Component) {
             return React.createElement(
                 'div',
                 { className: 'modal-body' },
+                React.createElement(SwitchButton, null),
                 React.createElement(
                     'ul',
-                    {
-                        id: 'signIn_signUp_tab',
-                        className: 'nav nav-tabs nav-justified',
-                        role: 'tablist'
-                    },
+                    { id: 'sign_in_up_tab', className: 'nav' },
                     React.createElement(
                         'li',
-                        { className: 'nav-item' },
-                        React.createElement(
-                            'a',
-                            {
-                                id: 'nav_signIn',
-                                className: 'nav-link active',
-                                'data-toggle': 'tab',
-                                href: '#content_signIn',
-                                role: 'tab',
-                                'aria-controls': 'content_signIn',
-                                'aria-selected': 'true'
-                            },
-                            'Sign In'
-                        )
+                        { className: 'active' },
+                        React.createElement('a', {
+                            id: 'loginTabLink',
+                            href: '#content_login',
+                            'data-toggle': 'tab'
+                        })
                     ),
                     React.createElement(
                         'li',
-                        { className: 'nav-item' },
-                        React.createElement(
-                            'a',
-                            {
-                                id: 'nav_signUp',
-                                className: 'nav-link',
-                                'data-toggle': 'tab',
-                                href: '#content_signUp',
-                                role: 'tab',
-                                'aria-controls': 'content_signUp',
-                                'aria-selected': 'true'
-                            },
-                            'Sign Up'
-                        )
+                        null,
+                        React.createElement('a', {
+                            id: 'registerTabLink',
+                            href: '#content_register',
+                            'data-toggle': 'tab'
+                        })
                     )
                 ),
                 React.createElement(
@@ -417,20 +401,18 @@ var LoginModalBody = function (_React$Component) {
                     React.createElement(
                         'div',
                         {
-                            id: 'content_signIn',
+                            id: 'content_login',
                             className: 'tab-pane fade show active',
-                            role: 'tabpanel',
-                            'aria-labelledby': 'content_signIn-tab'
+                            role: 'tabpanel'
                         },
                         React.createElement(LoginForm, null)
                     ),
                     React.createElement(
                         'div',
                         {
-                            id: 'content_signUp',
+                            id: 'content_register',
                             className: 'tab-pane fade',
-                            role: 'tabpanel',
-                            'aria-labelledby': 'content_signUp-tab'
+                            role: 'tabpanel'
                         },
                         React.createElement(RegisterForm, null)
                     )
@@ -440,6 +422,49 @@ var LoginModalBody = function (_React$Component) {
     }]);
 
     return LoginModalBody;
+}(React.Component);
+
+var SwitchButton = function (_React$Component2) {
+    _inherits(SwitchButton, _React$Component2);
+
+    function SwitchButton() {
+        _classCallCheck(this, SwitchButton);
+
+        return _possibleConstructorReturn(this, (SwitchButton.__proto__ || Object.getPrototypeOf(SwitchButton)).apply(this, arguments));
+    }
+
+    _createClass(SwitchButton, [{
+        key: 'componentDidMount',
+        value: function componentDidMount() {
+            $('#switchBtn').bootstrapToggle({
+                on: "Login",
+                onstyle: "primary",
+                off: "Register",
+                offstyle: "info"
+            }).change(function () {
+                var checked = $(this).prop('checked');
+                var tabLink = checked ? 'loginTabLink' : 'registerTabLink';
+
+                $('#sign_in_up_tab #' + tabLink).tab('show');
+            });
+        }
+    }, {
+        key: 'render',
+        value: function render() {
+            return React.createElement(
+                'div',
+                { id: 'switch_In_Up_container' },
+                React.createElement('input', {
+                    id: 'switchBtn',
+                    checked: 'true',
+                    readOnly: 'true',
+                    type: 'checkbox'
+                })
+            );
+        }
+    }]);
+
+    return SwitchButton;
 }(React.Component);
 
 module.exports = LoginModalBody;
@@ -30607,6 +30632,187 @@ module.exports = function(originalModule) {
 	return module;
 };
 
+
+/***/ }),
+
+/***/ "./plugins/switch-button/js/index.js":
+/*!*******************************************!*\
+  !*** ./plugins/switch-button/js/index.js ***!
+  \*******************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
+
+/*! ========================================================================
+ * Bootstrap Toggle: bootstrap-toggle.js v2.2.0
+ * http://www.bootstraptoggle.com
+ * ========================================================================
+ * Copyright 2014 Min Hur, The New York Times Company
+ * Licensed under MIT
+ * ======================================================================== */
+
++function ($) {
+	'use strict';
+
+	// TOGGLE PUBLIC CLASS DEFINITION
+	// ==============================
+
+	var Toggle = function Toggle(element, options) {
+		this.$element = $(element);
+		this.options = $.extend({}, this.defaults(), options);
+		this.render();
+	};
+
+	Toggle.VERSION = '2.2.0';
+
+	Toggle.DEFAULTS = {
+		on: 'On',
+		off: 'Off',
+		onstyle: 'primary',
+		offstyle: 'default',
+		size: 'normal',
+		style: '',
+		width: null,
+		height: null
+	};
+
+	Toggle.prototype.defaults = function () {
+		return {
+			on: this.$element.attr('data-on') || Toggle.DEFAULTS.on,
+			off: this.$element.attr('data-off') || Toggle.DEFAULTS.off,
+			onstyle: this.$element.attr('data-onstyle') || Toggle.DEFAULTS.onstyle,
+			offstyle: this.$element.attr('data-offstyle') || Toggle.DEFAULTS.offstyle,
+			size: this.$element.attr('data-size') || Toggle.DEFAULTS.size,
+			style: this.$element.attr('data-style') || Toggle.DEFAULTS.style,
+			width: this.$element.attr('data-width') || Toggle.DEFAULTS.width,
+			height: this.$element.attr('data-height') || Toggle.DEFAULTS.height
+		};
+	};
+
+	Toggle.prototype.render = function () {
+		this._onstyle = 'btn-' + this.options.onstyle;
+		this._offstyle = 'btn-' + this.options.offstyle;
+		var size = this.options.size === 'large' ? 'btn-lg' : this.options.size === 'small' ? 'btn-sm' : this.options.size === 'mini' ? 'btn-xs' : '';
+		var $toggleOn = $('<label class="btn">').html(this.options.on).addClass(this._onstyle + ' ' + size);
+		var $toggleOff = $('<label class="btn">').html(this.options.off).addClass(this._offstyle + ' ' + size + ' active');
+		var $toggleHandle = $('<span class="toggle-handle btn btn-default">').addClass(size);
+		var $toggleGroup = $('<div class="toggle-group">').append($toggleOn, $toggleOff, $toggleHandle);
+		var $toggle = $('<div class="toggle btn" data-toggle="toggle">').addClass(this.$element.prop('checked') ? this._onstyle : this._offstyle + ' off').addClass(size).addClass(this.options.style);
+
+		this.$element.wrap($toggle);
+		$.extend(this, {
+			$toggle: this.$element.parent(),
+			$toggleOn: $toggleOn,
+			$toggleOff: $toggleOff,
+			$toggleGroup: $toggleGroup
+		});
+		this.$toggle.append($toggleGroup);
+
+		var width = this.options.width || Math.max($toggleOn.outerWidth(), $toggleOff.outerWidth()) + $toggleHandle.outerWidth() / 2;
+		var height = this.options.height || Math.max($toggleOn.outerHeight(), $toggleOff.outerHeight());
+		$toggleOn.addClass('toggle-on');
+		$toggleOff.addClass('toggle-off');
+		this.$toggle.css({ width: width, height: height });
+		if (this.options.height) {
+			$toggleOn.css('line-height', $toggleOn.height() + 'px');
+			$toggleOff.css('line-height', $toggleOff.height() + 'px');
+		}
+		this.update(true);
+		this.trigger(true);
+	};
+
+	Toggle.prototype.toggle = function () {
+		if (this.$element.prop('checked')) this.off();else this.on();
+	};
+
+	Toggle.prototype.on = function (silent) {
+		if (this.$element.prop('disabled')) return false;
+		this.$toggle.removeClass(this._offstyle + ' off').addClass(this._onstyle);
+		this.$element.prop('checked', true);
+		if (!silent) this.trigger();
+	};
+
+	Toggle.prototype.off = function (silent) {
+		if (this.$element.prop('disabled')) return false;
+		this.$toggle.removeClass(this._onstyle).addClass(this._offstyle + ' off');
+		this.$element.prop('checked', false);
+		if (!silent) this.trigger();
+	};
+
+	Toggle.prototype.enable = function () {
+		this.$toggle.removeAttr('disabled');
+		this.$element.prop('disabled', false);
+	};
+
+	Toggle.prototype.disable = function () {
+		this.$toggle.attr('disabled', 'disabled');
+		this.$element.prop('disabled', true);
+	};
+
+	Toggle.prototype.update = function (silent) {
+		if (this.$element.prop('disabled')) this.disable();else this.enable();
+		if (this.$element.prop('checked')) this.on(silent);else this.off(silent);
+	};
+
+	Toggle.prototype.trigger = function (silent) {
+		this.$element.off('change.bs.toggle');
+		if (!silent) this.$element.change();
+		this.$element.on('change.bs.toggle', $.proxy(function () {
+			this.update();
+		}, this));
+	};
+
+	Toggle.prototype.destroy = function () {
+		this.$element.off('change.bs.toggle');
+		this.$toggleGroup.remove();
+		this.$element.removeData('bs.toggle');
+		this.$element.unwrap();
+	};
+
+	// TOGGLE PLUGIN DEFINITION
+	// ========================
+
+	function Plugin(option) {
+		return this.each(function () {
+			var $this = $(this);
+			var data = $this.data('bs.toggle');
+			var options = (typeof option === 'undefined' ? 'undefined' : _typeof(option)) == 'object' && option;
+
+			if (!data) $this.data('bs.toggle', data = new Toggle(this, options));
+			if (typeof option == 'string' && data[option]) data[option]();
+		});
+	}
+
+	var old = $.fn.bootstrapToggle;
+
+	$.fn.bootstrapToggle = Plugin;
+	$.fn.bootstrapToggle.Constructor = Toggle;
+
+	// TOGGLE NO CONFLICT
+	// ==================
+
+	$.fn.toggle.noConflict = function () {
+		$.fn.bootstrapToggle = old;
+		return this;
+	};
+
+	// TOGGLE DATA-API
+	// ===============
+
+	$(function () {
+		$('input[type=checkbox][data-toggle^=toggle]').bootstrapToggle();
+	});
+
+	$(document).on('click.bs.toggle', 'div[data-toggle^=toggle]', function (e) {
+		var $checkbox = $(this).find('input[type=checkbox]');
+		$checkbox.bootstrapToggle('toggle');
+		e.preventDefault();
+	});
+}(jQuery);
 
 /***/ }),
 
--- web/src/components/ui-components/login-modal-body.js
+++ web/src/components/ui-components/login-modal-body.js
@@ -1,65 +1,81 @@
 'use strict';
-
+/* global $ */
 const React = require('react');
 /* eslint-disable */
 const LoginForm = require('./login-modal-login-form');
 const RegisterForm = require('./login-modal-register-form');
+
+require('/plugins/switch-button/js/index.js');
 /* eslint-disable */
 
 class LoginModalBody extends React.Component {
     render() {
         return (
             <div className="modal-body">
-                <ul
-                    id="signIn_signUp_tab"
-                    className="nav nav-tabs nav-justified"
-                    role="tablist"
-                >
-                    <li className="nav-item">
+                <SwitchButton/>
+                <ul id="sign_in_up_tab" className="nav">
+                    <li className="active">
                         <a
-                            id="nav_signIn"
-                            className = "nav-link active"
+                            id="loginTabLink"
+                            href="#content_login"
                             data-toggle="tab"
-                            href="#content_signIn"
-                            role="tab"
-                            aria-controls="content_signIn"
-                            aria-selected="true"
-                        >
-                            Sign In
-                        </a>
+                        ></a>
                     </li>
-                    <li className="nav-item">
+                    <li>
                         <a
-                            id="nav_signUp"
-                            className = "nav-link"
+                            id="registerTabLink"
+                            href="#content_register"
                             data-toggle="tab"
-                            href="#content_signUp"
-                            role="tab"
-                            aria-controls="content_signUp"
-                            aria-selected="true"
-                        >
-                            Sign Up
-                        </a>
+                        ></a>
                     </li>
                 </ul>
                 <div className="tab-content">
                     <div
-                        id="content_signIn"
+                        id="content_login"
                         className = "tab-pane fade show active"
                         role="tabpanel"
-                        aria-labelledby="content_signIn-tab"
                     >
                         <LoginForm/>
                     </div>
                     <div
-                        id="content_signUp"
+                        id="content_register"
                         className = "tab-pane fade"
                         role="tabpanel"
-                        aria-labelledby="content_signUp-tab"
                     >
                         <RegisterForm/>
                     </div>
                 </div>
+            </div>
+        );
+    }
+}
+
+class SwitchButton extends React.Component {
+    componentDidMount() {
+        $('#switchBtn')
+            .bootstrapToggle({
+                on      : "Login",
+                onstyle : "primary",
+                off     : "Register",
+                offstyle: "info",
+            })
+            .change(function() {
+                var checked = $(this).prop('checked');
+                var tabLink = checked ? 'loginTabLink' : 'registerTabLink';
+
+                $(`#sign_in_up_tab #${tabLink}`).tab('show');
+            });
+    }
+
+    render() {
+        return (
+            <div id="switch_In_Up_container">
+                <input
+                    id="switchBtn"
+                    checked="true"
+                    readOnly="true"
+                    type="checkbox"
+                />
             </div>
         );
     }
